deb9fc309563d049ee93dba30e2986ad02b56e31
Muhammad Usman <usmanazhar357@gmail.com>
2025-01-20 12:52:33 +0500

    commit on 01-20-2025

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\ReloadlyService;
 
 class AdminController extends Controller
 {
+    protected $reloadlyService;
+
+    public function __construct(ReloadlyService $reloadlyService)
+    {
+        $this->reloadlyService = $reloadlyService;
+    }
+
     public function dashboard()
     {
+
+        return $this->reloadlyService->getBalance();
+
         $data['page_title'] = "Dashboard";
         return view('admin.dashboard', $data);
     }
--- app/Services/ReloadlyService.php
+++ app/Services/ReloadlyService.php
@@ -7,69 +7,173 @@ use GuzzleHttp\Client;
 class ReloadlyService
 {
     protected $client;
+    protected $clientId;
+    protected $clientSecret;
     protected $authUrl;
     protected $topupUrl;
     protected $giftcardUrl;
+    protected $utilityPaymentsUrl;
     protected $accessToken;
 
     public function __construct()
     {
         $this->client = new Client();
-        $this->authUrl = config('services.reloadly.auth_url', env('RELOADLY_AUTH_URL'));
-        $this->topupUrl = config('services.reloadly.topup_url', env('RELOADLY_TOPUP_URL'));
-        $this->giftcardUrl = config('services.reloadly.giftcard_url', env('RELOADLY_GIFTCARD_URL'));
+        $test_mode = env('RELOADLY_TEST_MODE');
+
+        if ($test_mode == 1) {
+            $this->clientId = '21spSJY7lgpIgXvyCHDI8jPOQZiEbbFo';
+            $this->clientSecret = 'WQpxgUsZz2-GalPK4cZ1iYCKJQfpZ3-nV0cbfRuWa8xkh78immBQ3vQKwilFQVc';
+            $this->authUrl = env('RELOADLY_AUTH_URL');
+            $this->topupUrl = 'https://topups-sandbox.reloadly.com';
+            $this->giftcardUrl = 'https://giftcards-sandbox.reloadly.com';
+            $this->utilityPaymentsUrl = 'https://utilities-sandbox.reloadly.com';
+        } else {
+            $this->clientId = env('RELOADLY_CLIENT_ID');
+            $this->clientSecret = env('RELOADLY_CLIENT_SECRET');
+            $this->authUrl = env('RELOADLY_AUTH_URL');
+            $this->topupUrl = env('RELOADLY_TOPUP_URL');
+            $this->giftcardUrl = env('RELOADLY_GIFTCARD_URL');
+            $this->utilityPaymentsUrl = env('RELOADLY_UTILITY_PAYMENTS_URL');
+        }
+    }
+
+    private function getUrl($type = 'topups')
+    {
+        if ($type == 'giftcards') {
+            $url = $this->giftcardUrl;
+        } else if ($type == 'utilities') {
+            $url = $this->utilityPaymentsUrl;
+        } else {
+            $url = $this->topupUrl;
+        }
+
+        return $url;
     }
 
     /**
      * Authenticate and get the access token.
      */
-    public function authenticate()
+    public function authenticate($type = 'topups')
     {
-        $response = $this->client->post($this->authUrl, [
-            'form_params' => [
-                'client_id' => env('RELOADLY_CLIENT_ID'),
-                'client_secret' => env('RELOADLY_CLIENT_SECRET'),
-                'grant_type' => 'client_credentials',
-                'audience' => $this->topupUrl,
-            ],
-        ]);
+        $url = $this->getUrl($type);
 
-        $data = json_decode($response->getBody(), true);
-        $this->accessToken = $data['access_token'];
+        try {
+            $response = $this->client->post($this->authUrl, [
+                'form_params' => [
+                    'client_id' => $this->clientId,
+                    'client_secret' => $this->clientSecret,
+                    'grant_type' => 'client_credentials',
+                    'audience' => $url,
+                ],
+            ]);
 
-        return $this->accessToken;
+            $data = json_decode($response->getBody(), true);
+            $this->accessToken = $data['access_token'];
+
+            return $this->accessToken;
+        } catch (\Exception $e) {
+            return false;
+        }
     }
 
     /**
      * Fetch available countries.
      */
-    public function getCountries()
+    public function getCountries($type = 'topups')
     {
-        $this->authenticate();
+        $url = $this->getUrl($type);
 
-        $response = $this->client->get("{$this->topupUrl}/countries", [
-            'headers' => [
-                'Authorization' => "Bearer {$this->accessToken}",
-            ],
-        ]);
+        try {
+            $this->authenticate($type);
 
-        return json_decode($response->getBody(), true);
+            $response = $this->client->get("{$url}/countries", [
+                'headers' => [
+                    'Authorization' => "Bearer {$this->accessToken}",
+                ],
+            ]);
+
+            $data = json_decode($response->getBody(), true);
+            $output = ['success' => 1, 'data' => $data];
+        } catch (\Exception $e) {
+            $output = ['success' => 0, 'msg' => $e->getMessage()];
+        }
+
+        return $output;
     }
 
     /**
      * Fetch available mobile operators.
      */
-    public function getOperators()
+    public function getOperators($type = 'topups')
     {
-        $this->authenticate();
+        $url = $this->getUrl($type);
 
-        $response = $this->client->get("{$this->topupUrl}/operators", [
-            'headers' => [
-                'Authorization' => "Bearer {$this->accessToken}",
-            ],
-        ]);
+        try {
+            $this->authenticate($type);
 
-        return json_decode($response->getBody(), true);
+            $response = $this->client->get("{$url}/operators", [
+                'headers' => [
+                    'Authorization' => "Bearer {$this->accessToken}",
+                ],
+            ]);
+
+            $data = json_decode($response->getBody(), true);
+            $output = ['success' => 1, 'data' => $data];
+        } catch (\Exception $e) {
+            $output = ['success' => 0, 'msg' => $e->getMessage()];
+        }
+
+        return $output;
+    }
+
+    /**
+     * Fetch mobile operators by operator Id.
+     */
+    public function getOperatorByID($operatorId, $type = 'topups')
+    {
+        $url = $this->getUrl($type);
+
+        try {
+            $this->authenticate($type);
+
+            $response = $this->client->get("{$url}/operators/{$operatorId}", [
+                'headers' => [
+                    'Authorization' => "Bearer {$this->accessToken}",
+                ],
+            ]);
+
+            $data = json_decode($response->getBody(), true);
+            $output = ['success' => 1, 'data' => $data];
+        } catch (\Exception $e) {
+            $output = ['success' => 0, 'msg' => $e->getMessage()];
+        }
+
+        return $output;
+    }
+
+    /**
+     * Fetch mobile operators by ISO Code.
+     */
+    public function getOperatorByISOCode($isoCode, $type = 'topups')
+    {
+        $url = $this->getUrl($type);
+
+        try {
+            $this->authenticate($type);
+
+            $response = $this->client->get("{$url}/operators/countries/{$isoCode}", [
+                'headers' => [
+                    'Authorization' => "Bearer {$this->accessToken}",
+                ],
+            ]);
+
+            $data = json_decode($response->getBody(), true);
+            $output = ['success' => 1, 'data' => $data];
+        } catch (\Exception $e) {
+            $output = ['success' => 0, 'msg' => $e->getMessage()];
+        }
+
+        return $output;
     }
 
     /**
@@ -77,40 +181,55 @@ class ReloadlyService
      */
     public function sendAirtime($operatorId, $amount, $countryCode, $phoneNumber)
     {
-        $this->authenticate();
+        try {
+            $this->authenticate();
 
-        $response = $this->client->post("{$this->topupUrl}/topups", [
-            'headers' => [
-                'Authorization' => "Bearer {$this->accessToken}",
-                'Content-Type' => 'application/json',
-            ],
-            'json' => [
-                'operatorId' => $operatorId,
-                'amount' => $amount,
-                'recipientPhone' => [
-                    'countryCode' => $countryCode,
-                    'number' => $phoneNumber,
+            $response = $this->client->post("{$this->topupUrl}/topups", [
+                'headers' => [
+                    'Authorization' => "Bearer {$this->accessToken}",
+                    'Content-Type' => 'application/json',
                 ],
-                'customIdentifier' => uniqid('txn_'),
-            ],
-        ]);
+                'json' => [
+                    'operatorId' => $operatorId,
+                    'amount' => $amount,
+                    'recipientPhone' => [
+                        'countryCode' => $countryCode,
+                        'number' => $phoneNumber,
+                    ],
+                    'customIdentifier' => uniqid('txn_'),
+                ],
+            ]);
 
-        return json_decode($response->getBody(), true);
+            $data = json_decode($response->getBody(), true);
+            $output = ['success' => 1, 'data' => $data];
+        } catch (\Exception $e) {
+            $output = ['success' => 0, 'msg' => $e->getMessage()];
+        }
+
+        return $output;
     }
 
     /**
      * Get account balance.
      */
-    public function getBalance()
+    public function getBalance($type = 'topups')
     {
-        $this->authenticate();
+        $url = $this->getUrl($type);
+        $this->authenticate($type);
 
-        $response = $this->client->get("{$this->topupUrl}/accounts/balance", [
-            'headers' => [
-                'Authorization' => "Bearer {$this->accessToken}",
-            ],
-        ]);
+        try {
+            $response = $this->client->get("{$url}/accounts/balance", [
+                'headers' => [
+                    'Authorization' => "Bearer {$this->accessToken}",
+                ],
+            ]);
 
-        return json_decode($response->getBody(), true);
+            $data = json_decode($response->getBody(), true);
+            $output = ['success' => 1, 'data' => $data];
+        } catch (\Exception $e) {
+            $output = ['success' => 0, 'msg' => $e->getMessage()];
+        }
+
+        return $output;
     }
 }
