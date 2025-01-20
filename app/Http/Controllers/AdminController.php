6e3804205247e3c256893ef24d2c32f79fc425e9
Muhammad Usman <usmanazhar357@gmail.com>
2025-01-20 14:53:47 +0500

    commit on 01-20-2025

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,22 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Services\ReloadlyService;
 
 class AdminController extends Controller
 {
-    protected $reloadlyService;
-
-    public function __construct(ReloadlyService $reloadlyService)
-    {
-        $this->reloadlyService = $reloadlyService;
-    }
-
     public function dashboard()
     {
-
-        return $this->reloadlyService->getBalance();
-
         $data['page_title'] = "Dashboard";
         return view('admin.dashboard', $data);
     }
